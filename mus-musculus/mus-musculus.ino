9518b388b8409accfa399cb87fade5e574e1cb26
Cédric Nanteau <cedric.nanteau@gmail.com>
2019-01-18 10:40:20 +0100

    Collecting the data from the trackpad and send it to the computer (via wire) as the arduino is a PS/2 mouse works

--- mus-musculus/mus-musculus.ino
+++ mus-musculus/mus-musculus.ino
@@ -1,70 +1,20 @@
-//#include <ps2.h>
 #include "PS2Mouse.h"
 #include "Mouse.h"
 
 /*
-* an arduino sketch to interface with a ps/2 mouse.
-* Also uses serial protocol to talk back to the host
-* and report what it finds.
-*/
-
-/*
-* Pin 5 is the mouse data pin, pin 6 is the clock pin
+* Pin 10 is the mouse data pin, pin 9 is the clock pin
 * Feel free to use whatever pins are convenient.
 */
 #define P_DATA 10
 #define P_CLK 9
 PS2Mouse mouse(P_CLK, P_DATA);
 
-/*
-* initialize the mouse. Reset it, and place it into remote
-* mode, so we can get the encoder data on demand.
-*/
-void mouse_init() {
-    /*
-    mouse.write(0xff);  // reset
-    mouse.read();  // ack byte
-    mouse.read();  // blank
-    mouse.read();  // blank
-    mouse.write(0xf0);  // remote mode
-    mouse.read();  // ack
-    delayMicroseconds(100);
-    */
-}
-
-
 void setup() {
     Serial.begin(9600);
     while(!Serial);
-    mouse_init();
     Mouse.begin();
 }
-
-/*
-* get a reading from the mouse and report it back to the
-* host via the serial line.
-*/
 void loop() {
-    /*
-    char mstat;
-    char mx;
-    char my;
-
-    /* get a reading from the mouse
-    mouse.write(0xeb);  // give me data!
-    mouse.read();      // ignore ack
-    mstat = mouse.read();
-    mx = mouse.read();
-    my = mouse.read();
-
-    /* send the data back up
-    Serial.print(mstat, BIN);
-    Serial.print("\tX=");
-    Serial.print(mx, DEC);
-    Serial.print("\tY=");
-    Serial.print(my, DEC);
-    Serial.println();
-    //  delay(20);  /* twiddle */
 
     MouseData data = mouse.readData();
 
